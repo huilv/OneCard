edc6a1d7ab7f82ec64cb2ec71f5c4e528a64134c
lvhui <lvhui@Ctrip.com>
2022-07-10 21:53:59 +0800

    feat: 保存bitmap到本地

--- onecard/src/main/java/com/datapay/onecard/utils/BitmapUtil.java
+++ onecard/src/main/java/com/datapay/onecard/utils/BitmapUtil.java
@@ -5,6 +5,8 @@ import android.annotation.SuppressLint;
 import android.content.ContentUris;
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 
@@ -14,24 +16,30 @@ import android.provider.MediaStore;
 
 import androidx.core.content.FileProvider;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class BitmapUtil {
     private static BitmapUtil mBitmapUtil;
-    private BitmapUtil(){
+
+    private BitmapUtil() {
     }
 
-    public static BitmapUtil getInstance(){
-        if(mBitmapUtil==null){
-            mBitmapUtil=new BitmapUtil();
+    public static BitmapUtil getInstance() {
+        if (mBitmapUtil == null) {
+            mBitmapUtil = new BitmapUtil();
         }
-        return  mBitmapUtil;
+        return mBitmapUtil;
     }
 
-    public String getCameraPath(Context context){
-        String  mFilePath = SDCardUtil.getInnerSDCardPath(context) + "/oneCard/image";
+    public String getCameraPath(Context context) {
+        String mFilePath = SDCardUtil.getInnerSDCardPath(context) + "/oneCard/image";
         File outFilePath = new File(mFilePath);
         if (!outFilePath.exists()) {
             outFilePath.mkdirs();
@@ -120,17 +128,17 @@ public class BitmapUtil {
             }
             if (cursor != null) {
                 cursor.close();
-                cursor=null;
+                cursor = null;
             }
         } catch (Exception e) {
             if (cursor != null) {
                 cursor.close();
-                cursor=null;
+                cursor = null;
             }
-        }finally {
+        } finally {
             if (cursor != null) {
                 cursor.close();
-                cursor=null;
+                cursor = null;
             }
         }
         return path;
@@ -151,4 +159,80 @@ public class BitmapUtil {
     private static boolean isDownloadsDocument(Uri uri) {
         return "com.android.providers.downloads.documents".equals(uri.getAuthority());
     }
+
+    /**
+     * 质量压缩
+     */
+    public Bitmap compressImage(Bitmap image) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);// 质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
+        int options = 90;
+        while (baos.toByteArray().length / 1024 > 1024) { // 循环判断如果压缩后图片是否大于1024kb,大于继续压缩
+            baos.reset(); // 重置baos即清空baos
+            image.compress(Bitmap.CompressFormat.JPEG, options, baos);// 这里压缩options%，把压缩后的数据存放到baos中
+            options -= 10;// 每次都减少10
+        }
+        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());// 把压缩后的数据baos存放到ByteArrayInputStream中
+        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);// 把ByteArrayInputStream数据生成图片
+        return bitmap;
+    }
+
+    /**
+     * 大小压缩
+     */
+    public Bitmap compressBitmap(Bitmap image) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());
+        BitmapFactory.Options newOpts = new BitmapFactory.Options();
+        // 开始读入图片，此时把options.inJustDecodeBounds 设回true了
+        newOpts.inJustDecodeBounds = true;
+        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, newOpts);
+        newOpts.inJustDecodeBounds = false;
+        int w = newOpts.outWidth;
+        int h = newOpts.outHeight;
+        float hh = 1000f;
+        float ww = 600f;
+        // 缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
+        int be = 1;// be=1表示不缩放
+        if (w > h && w > ww) {// 如果宽度大的话根据宽度固定大小缩放
+            be = (int) (newOpts.outWidth / ww);
+        } else if (w < h && h > hh) { // 如果高度高的话根据高度固定大小缩放
+            be = (int) (newOpts.outHeight / hh);
+        }
+        if (be <= 0) {
+            be = 1;
+        }
+        newOpts.inSampleSize = be; // 设置缩放比例
+        // newOpts.inPreferredConfig = Config.RGB_565;//降低图片从ARGB888到RGB565
+        // 重新读入图片，注意此时已经把options.inJustDecodeBounds 设回false了
+        isBm = new ByteArrayInputStream(baos.toByteArray());
+        bitmap = BitmapFactory.decodeStream(isBm, null, newOpts);
+        return compressImage(bitmap);// 压缩好比例大小后再进行质量压缩
+    }
+
+    /**
+     * Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+     * Uri uri = Uri.fromFile(file);
+     * intent.setData(uri);
+     * context.sendBroadcast(intent);
+     * 这个广播的目的就是更新图库，发了这个广播进入相册就可以找到你保存的图片了！，记得要传你更新的file哦
+     */
+    public static void saveBitmap(String name, Bitmap bm, Context mContext) {
+        String TargetPath = SDCardUtil.getInnerSDCardPath(mContext)+ "/oneCard/image";
+        File file = new File(TargetPath);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        File saveFile = new File(TargetPath, name);
+        try {
+            FileOutputStream saveImgOut = new FileOutputStream(saveFile);
+            bm.compress(Bitmap.CompressFormat.JPEG, 100, saveImgOut);
+            saveImgOut.flush();
+            saveImgOut.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
